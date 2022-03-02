a7f66aecc1f52f5dd345389a9a36e74b409ad26b
Can Burak S <canburak@msn.com>
2022-03-02 17:03:16 +0300

    ..

--- components/styled/index.tsx
+++ components/styled/index.tsx
@@ -38,7 +38,7 @@ export const PostHeader = ({ title, description, topics, modified }) => (
     <Box css={{ width: '100%', maxWidth: 720, margin: '0 15px' }}>
         <HeaderOne css={{ fontWeight: 500 }} className="animate-text-md">{title}</HeaderOne>
         <Text className="animate-text-lg text-sm mt-4" css={{fontSize:16, opacity:0.7}}>UPDATED: <time dateTime={modified}>{modified}</time></Text>
-        <Separator css={{ margin: '15px 0' }} className="animate-text-xl" />
+        <Separator css={{ margin: '24px 0' }} className="animate-text-xl" />
         <Text className="animate-text-xl">{description}</Text>
         <Flex css={{ height: 20, alignItems: 'center', marginTop:32 }}>
             {topics.map((cat, index) => (
