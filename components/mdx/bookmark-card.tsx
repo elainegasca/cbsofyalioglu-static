9dace99af9a71282f46320a224d1cb15191a26b0
Can Burak S <canburak@msn.com>
2023-05-18 02:26:25 +0300

    update: bookmark card

--- components/mdx/bookmark-card.tsx
+++ components/mdx/bookmark-card.tsx
@@ -1,19 +1,32 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
+const imageLoader = ({ src, width, quality }) => {
+    return `${src}?w=${width}&q=${quality || 75}`;
+  };
 export const BookmarkCard = ({ title, description, url, image }) => {
     return (
-        <div className="bookmark-card flex items-stretch justify-between border border-gray-700 rounded-sm bg-[rgba(255,255,255,0.1)] h-25 max-h-25 w-full relative box-border group p-1 grow-[3] md:grow-[2]">
-            <div className="flex flex-col justify-start  p-1 lg:p-2">
+        <div className="bookmark-card flex flex-row items-stretch justify-between border border-gray-700 rounded-sm bg-[rgba(255,255,255,0.1)] h-25 max-h-25 w-full relative box-border group my-4">
+            <div className="flex flex-col justify-start  p-1 lg:p-2  flex-grow-[4] md:flex-grow-[5]">
                 <Link href={url}>
                     <h4 className='text-sm sm:text-lg lg:text-xl sm:mb-1 lg:mb-2 font-semibold mb-0 hover:underline bookmark-card-title z-10'>{title}</h4>
                 </Link>
                 <p className='bookmark-card-description text-xs sm:text-sm !mt-0'>{description}</p>
             </div>
             <div
-                className="flex flex-col justify-center items-center h-full box-border grow-[1] z-10 relative"
-                style={{  overflow: 'hidden', maxWidth: '30%' }}
+                className="flex flex-col justify-center items-center h-full box-border flex-grow-[] z-20 relative !max-w-40 border border-solid border-gray-700 rounded-sm overflow-hidden group-hover:border-gray-400"
+                style={{  minWidth: "20%", maxWidth:150, minHeight:"100px" }}
             >
-                <img src={image} alt={title} style={{ height: '100%', minWidth: '110%', margin:0, marginLeft:"-5%", marginRight:"-5%", borderRadius:0, maxHeight:100 }} className='z-10' />
+                <Image 
+                fill 
+                loader={imageLoader} 
+                src={image} 
+                alt={title} 
+                //style={{ height: '100%', minWidth: '110%', margin:0, marginLeft:"-5%", marginRight:"-5%", borderRadius:0, maxHeight:100 }} 
+                className='z-10 !rounded-none opacity-80 group-hover:opacity-100 duration-200 transform ease-linear' 
+                loading="lazy" 
+                
+                />
             </div>
             <Link href={url} className='!absolute bottom-0 left-0 right-0 top-0 min-w-full duration-200 transform ease-linear group-hover:bg-[rgba(255,255,255,0.05)] z-10 !m-0'></Link>
         </div>
